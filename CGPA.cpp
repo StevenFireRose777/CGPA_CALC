df8132c3040236773c0304a5b24bd4dd42333977
StevenFireRose777 <stevennnuno769@gmail.com>
2025-04-25 23:04:59 -0700

    check point 5: Updated .cpp file

--- CGPA.cpp
+++ CGPA.cpp
@@ -1,52 +1,54 @@
 #include "CGPA.hpp"
+#include <iomanip>
 
+// Basic CGPA C++ program, not the best one, just for learning classes
 
-student_GPA::student_GPA(){
+student_GPA::student_GPA(){ // Constructor
 
     std::cout << "What is your name: ";
     std::cin >> name;
 
     std::cout << "How many semesters did you have so far: ";
     std::cin >> semesters;
+    // Get input values 
 
-    GPAS.resize(semesters);
+    GPAS.resize(semesters); // Adjust vector size to num of semesters
     
 
     for(int i = 0; i < semesters; ++i){
         std::cout << "Enter GPA for semester " << (i + 1) << ": ";
         std::cin >> GPAS[i];
-    }
+    } // Let user put in GPA for each number of semesters
     
 
 }
 
 student_GPA::~student_GPA()
 {
-    std::cout << "Class destroyed SUCCESSFULLY\n";
+    std::cout << "Class destroyed SUCCESSFULLY\n"; // Destroy class and object (Just there to learn)
 }
 
 
 void calc_GPA(const std::vector<double>& GPAS, int semesters, std::string name){
     double total, average;
-    for(auto i = 0; i < semesters; ++i){
-        total += GPAS[i];
+    for(auto i = 0; i < semesters; ++i){ // auto = "Yo compiler, initalize to this number"
+        total += GPAS[i]; // For every GPA index, add it to entire decimal total
     }
-    average = total / semesters;
+    average = total / semesters; // Get total GPA
 
-    std:: cout << name + " your cummunative GPA in " << semesters << " semesters is about " << average << "\n";
+    std:: cout << name + " your cummunative GPA in " << semesters << " semesters is about " << std::fixed << std::setprecision(3) << average << "\n";
 
-
-
+    // Prints out name + cummunative GPA with decimal point precision of 3 places.
 }
 
 
 
 int main(){
 
-    student_GPA student;
+    student_GPA student; // object that will call constructer automatically
 
 
-    calc_GPA(student.GPAS, student.semesters, student.name);
+    calc_GPA(student.GPAS, student.semesters, student.name); // function call after constructor
 
     
     return 0;
