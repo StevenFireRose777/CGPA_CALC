6ccb33f3f8e52f48c2b4240dec2dce3f714ef09a
StevenFireRose777 <stevennnuno769@gmail.com>
2025-04-25 21:59:17 -0700

    check #1

--- CGPA.cpp
+++ CGPA.cpp
@@ -2,7 +2,35 @@
 #include <vector>
 
 
+class student_GPA{
+public:
+    student_GPA(std::string name, std::vector<int> GPAS);
+    ~student_GPA();
+};
+
+student_GPA::student_GPA(std::string name = "NULL", std::vector<int> GPAS)
+{
+    std::cout << "What is your name: ";
+    std::cin >> name;
+
+    int semseters;
+    std::cout << "How many semesters did you have so far: ";
+    std::cin >> semseters;
+
+}
+
+student_GPA::~student_GPA()
+{
+    std::cout << "Class destroyed SUCCESSFULLY\n";
+}
+
+
 
 int main(){
 
+    student_GPA();
+
+
+    
+    return 0;
 }
